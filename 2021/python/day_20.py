d9376bf520593ac36f6f557f2159150429dd05e7
Hasan Ibraheem <hasantiny@gmail.com>
2021-12-21 12:17:21 -0500

    I am speed

--- 2021/python/day_20.py
+++ 2021/python/day_20.py
@@ -13,22 +13,35 @@ def solve(input_str):
     R = len(img)
     C = len(img[0])
 
-    @cache
-    def get(r, c, step):
-        if step == 0:
-            return (r,c) in imm
+    steps = 50
+    goalr = (-(steps+1), R+(steps+1))
+    goalc = (-(steps+1), C+(steps+1))
 
+    startr = (goalr[0]-steps, goalr[1]+steps)
+    startc = (goalc[0]-steps, goalc[1]+steps)
+
+    images = []
+    start_img = [[(r,c) in imm for c in range(startc[0], startc[1])] for r in range(startr[0], startr[1])]
+
+    images.append(start_img)
+
+    def enhance(r,c, prev):
         num = 0
-        for br in (r-1,r,r+1):
-            for bc in (c-1,c,c+1):
-                num = (num << 1) | get(br, bc, step-1)
+
+        for br in (r,r+1,r+2):
+            for bc in (c,c+1,c+2):
+                num = (num << 1) | prev[br][bc]
 
         return enhl[num]
 
-    def count_on(step):
-        return sum(get(r,c,step) for r in range(-(step+1), R+(step+1)) for c in range(-(step+1), C+(step+1)))
+    for _ in range(steps):
+        nr = len(images[-1]) - 2
+        nc = len(images[-1][0]) - 2
+
+        new_img = [[enhance(r,c,images[-1]) for c in range(nc)] for r in range(nr)]
+        images.append(new_img)
     
-    return (count_on(2), count_on(50))
+    return (sum(map(sum, images[2])), sum(map(sum, images[50])))
 
 def main():
     answer = solve(get_input(__file__))
