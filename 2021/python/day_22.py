b1a938fbe0ba4b45d3bef97acdea67643530745d
Hasan Ibraheem <hasantiny@gmail.com>
2021-12-22 12:57:43 -0500

    Add 2021 day 22 🔳🔲

---
+++ 2021/python/day_22.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+
+from aoc import *
+from typing import NamedTuple
+import time
+
+def solve(input_str):
+    t1 = time.time()
+
+
+    cubes = []
+    cubes2 = []
+
+    for l in input_str.strip().split('\n'):
+        on = l[1] == 'n'
+        xmin, xmax, ymin, ymax, zmin, zmax = ints(l)
+        c = Cube(Range(xmin, xmax), Range(ymin, ymax), Range(zmin, zmax))
+
+        new_cubes = []
+        for other in cubes:
+            if other.intersects(c):
+                new_cubes.extend(other - c)
+            else:
+                new_cubes.append(other)
+
+        if on:
+            new_cubes.append(c)
+
+        cubes = new_cubes
+
+
+        xmin, ymin, zmin = [max(-50, e) for e in (xmin, ymin, zmin)]
+        xmax, ymax, zmax = [min(e, 50) for e in (xmax, ymax, zmax)]
+        c2 = Cube(Range(xmin, xmax), Range(ymin, ymax), Range(zmin, zmax))
+        if c2.unwrap() == None:
+            continue
+
+        new_cubes2 = []
+        for other in cubes2:
+            if other.intersects(c2):
+                new_cubes2.extend(other - c2)
+            else:
+                new_cubes2.append(other)
+
+        if on:
+            new_cubes2.append(c2)
+
+
+        cubes2 = new_cubes2
+
+    part2 = sum(c.volume() for c in cubes)
+    part1 = sum(c.volume() for c in cubes2)
+
+    t2 = time.time()
+
+    print(t2 - t1)
+
+    return (part1, part2)
+
+
+class Range(NamedTuple):
+    min: int
+    max: int
+
+    def size(self):
+        return self.max - self.min + 1
+
+    def intersects(self, other):
+        if self.min <= other.min <= self.max or self.min <= other.max <= self.max:
+            return True
+        if other.min <= self.min <= other.max or other.min <= self.max <= other.max:
+            return True
+        return False
+
+    def segment(self, other):
+        result = []
+        antiresult = []
+
+        # S O S O
+        if self.min <= other.min <= self.max <= other.max:
+            result.append(Range(self.min, other.min - 1))
+            antiresult.append(Range(other.min, self.max))
+
+        # O S O S
+        elif other.min <= self.min <= other.max <= self.max:
+            antiresult.append(Range(self.min, other.max))
+            result.append(Range(other.max + 1, self.max))
+
+        # S O O S
+        elif self.min <= other.min <= other.max <= self.max:
+            result.append(Range(self.min, other.min - 1))
+            antiresult.append(Range(other.min, other.max))
+            result.append(Range(other.max + 1, self.max))
+
+        # O S S O
+        elif other.min <= self.min <= self.max <= other.max:
+            antiresult.append(Range(self.min, self.max))
+
+        else:
+            result.append(self)
+
+        return (
+            [r for r in map(Range.unwrap, result) if r],
+            [a for a in map(Range.unwrap, antiresult) if a]
+        )
+
+    def unwrap(self):
+        if self.max < self.min:
+            return None
+        else:
+            return self
+
+
+class Cube(NamedTuple):
+    x: Range
+    y: Range
+    z: Range
+
+    def volume(self):
+        return self.x.size() * self.y.size() * self.z.size()
+
+    def intersects(self, other):
+        return self.x.intersects(other.x) and self.y.intersects(other.y) and self.z.intersects(other.z)
+    
+    def subtract(self, other):
+        xs, (antix,) = self.x.segment(other.x)
+        ys, (antiy,) = self.y.segment(other.y)
+        zs, (antiz,) = self.z.segment(other.z)
+        result = []
+
+        for x in (*xs, antix):
+            for y in (*ys, antiy):
+                for z in (*zs, antiz):
+                    if (x,y,z) == (antix,antiy,antiz): continue
+                    result.append(Cube(x,y,z))
+
+        return [r for r in map(Cube.unwrap, result) if r]
+
+    def unwrap(self):
+        if not self.x.unwrap() or not self.y.unwrap() or not self.z.unwrap():
+            return None
+        else:
+            return self
+
+    def __sub__(self, other):
+        return self.subtract(other)
+
+
+
+def main():
+    answer = solve(get_input(__file__))
+    print(answer)
+    assert answer == (524792, 1213461324555691)
+
+if __name__ == '__main__':
+    main()
+
