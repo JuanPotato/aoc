42bb6e1a5a1bbe2e885f651a0b59e7c0577ec316
Hasan Ibraheem <hasantiny@gmail.com>
2022-12-03 17:45:42 -0500

    write better day 3 code

--- 2022/day_03.py
+++ 2022/day_03.py
@@ -6,14 +6,12 @@ def solve(input_str):
     p1, p2 = 0, 0
 
     for r in parse_list(input_str):
-        r1 = Counter(r[:len(r)//2])
-        r2 = Counter(r[len(r)//2:])
-        com = Counter(r1) & Counter(r2)
+        com = set(r[:len(r)//2]) & set(r[len(r)//2:])
         for k in com:
             p1 += ord(k) - (ord('A') - 27 if k.isupper() else ord('a') - 1)
 
     for r1, r2, r3 in chunk(parse_list(input_str), 3):
-        com = Counter(r1) & Counter(r2) & Counter(r3)
+        com = set(r1) & set(r2) & set(r3)
         for k in com:
             p2 += ord(k) - (ord('A') - 27 if k.isupper() else ord('a') - 1)
 
