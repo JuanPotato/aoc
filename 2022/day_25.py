a55190ec7dffe530fb76862c023bb0337728099a
Hasan Ibraheem <hasantiny@gmail.com>
2022-12-25 01:16:03 -0500

    Better 2022 25, thanks @turquoise-hexagon

--- 2022/day_25.py
+++ 2022/day_25.py
@@ -3,47 +3,29 @@
 from aoc import *
 import math
 
+M = {'=': -2, '-': -1, '0': 0, '1': 1, '2': 2}
+def snafu_to_int(n):
+    r = 0
+
+    for i in n:
+        r = r * 5 + M[i]
+
+    return r
+
+def int_to_snafu(n):
+    r = ""
+
+    while n != 0:
+        m = n % 5
+        m = m - (m > 2) * 5
+        n = (n - m) // 5
+        r = '=-012'[m + 2] + r
+
+    return r
+
 def solve(input_str):
-    m = {
-        '2': 2,
-        '1': 1,
-        '0': 0,
-        '-': -1,
-        '=': -2
-    }
-
-    s = 0
-    for l in lines(input_str):
-        b = 1
-        n = 0
-        for d in l[::-1]:
-            n += m[d] * b
-            b *= 5
-        s += n
-
-    return snafu(s)
-
-def snafu(n):
-    digits = 1
-    max_num = lambda d: sum(2*5**i for i in range(d))
-    while max_num(digits) < n:
-        digits += 1
-
-    num = []
-    m = 0
-    for i in make_range(digits-1, 0):
-        nd = 0
-        err = math.inf
-        for k in (-2, -1, 0, 1, 2):
-            v = k * 5**i
-            nerr = abs(n - (m + v))
-            if nerr < err:
-                err = nerr
-                nd = k
-        m += nd * 5**i
-        num.append(nd)
-    return ''.join('=-012'[o+2] for o in num)
-
+    s = sum(map(snafu_to_int, lines(input_str)))
+    return int_to_snafu(s)
 
 def main():
     answer = solve(get_input(__file__))
@@ -52,5 +34,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
