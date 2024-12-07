625fcdbe8d98e18eb77fb776982761099c2fa778
Hasan Ibraheem <hasan@juanpota.to>
2024-12-06 21:12:45 -0500

    I made too changes that made it slower and then i just hit undo until it was the right answer and it was faster now

--- 2024/day_06.py
+++ 2024/day_06.py
@@ -5,29 +5,28 @@ from aoc import *
 
 def solve(input_str: str):
     grid = list(parse_list(input_str, list))
-    part1, visited = travel([list(r) for r in grid], True)
+    x, y = next(
+        (x, y) for y, row in enumerate(grid) for x, val in enumerate(row) if val == "^"
+    )
+
+    visited = travel([list(r) for r in grid], x, y, True)
+    part1 = len(visited)
 
     part2 = 0
     for c, r in visited:
         if grid[r][c] == ".":
             new_grid = [list(r) for r in grid]
             new_grid[r][c] = "#"
-            if not travel(new_grid):
+            if not travel(new_grid, x, y):
                 part2 += 1
     return part1, part2
 
 
-def travel(grid, part1=False):
-    x, y = next(
-        (x, y) for y, row in enumerate(grid) for x, val in enumerate(row) if val == "^"
-    )
-
-    dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-    nums = [1, 2, 4, 8]
+def travel(grid, x, y, part1=False):
+    dirs = ((0, -1), (1, 0), (0, 1), (-1, 0))
     d = 0
-    grid[y][x] = nums[d]
+    grid[y][x] = 1 << d
 
-    loop = False
     while 0 <= x < len(grid[0]) and 0 <= y < len(grid):
         dx, dy = dirs[d]
         if not (0 <= x + dx < len(grid[0]) and 0 <= y + dy < len(grid)):
@@ -41,11 +40,11 @@ def travel(grid, part1=False):
                 y += dy
                 x += dx
                 if v == ".":
-                    grid[y][x] = nums[d]
+                    grid[y][x] = 1 << d
                 else:
-                    if grid[y][x] & nums[d]:
+                    if grid[y][x] & (1 << d):
                         return None
-                    grid[y][x] |= nums[d]
+                    grid[y][x] |= 1 << d
 
     if not part1:
         return True
@@ -55,7 +54,7 @@ def travel(grid, part1=False):
         for x, v in enumerate(row)
         if isinstance(v, int)
     ]
-    return sum(isinstance(v, int) for r in grid for v in r), visited
+    return visited
 
 
 def main():
